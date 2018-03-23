ef1f55330860bc74453ff77c4522b96d7c80bfc1
Daniele Demichelis <d.demichelis@techgap.it>
2018-03-24 00:03:28 +0100

    Added support for graphical shell

--- lxde.sh
+++ lxde.sh
@@ -2,3 +2,15 @@
 
 # This install 400Mb
 sudo apt-get install --yes lxde-core
+
+# then startlxde
+# according to https://forum.lxde.org/viewtopic.php?f=1&t=1715
+
+# lxde cannot open display
+
+sudo apt-get install xinit
+startx
+
+# then install
+sudo apt-get install rxvt-unicode
+sudo apt-get install xterm
